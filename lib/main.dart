31a0d9af2985a73250bdb0f43eb94570a6ad424f
Jarkeer <83051761+Jarkeer@users.noreply.github.com>
2026-03-27 13:01:32 -0300

    sirve

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Text( "hOLA MUNDO"),
+      home: const Text( "Hola Mundo"),
     );
   }
 }
