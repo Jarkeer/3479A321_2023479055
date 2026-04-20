6711982a470c42833e9fa2446d8614781acd7290
Jarkeer <83051761+Jarkeer@users.noreply.github.com>
2026-04-20 18:43:12 -0400

    4to commit
    - Se creo una nueva carpeta en lib/model
    
    - Despues en widget dos nuevios archivos llamados: game_result_dart y game_result_ui
    - Se uso card
    - en historial se ve una partida ya jugada

--- lib/ui/screens/history_screen.dart
+++ lib/ui/screens/history_screen.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/models/game_result.dart';
+import 'package:flutter_app/ui/widgets/game_result_ui.dart';
 
 class HistoryScreen extends StatelessWidget {
   const HistoryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
+    // Resultado de prueba
+    final game = GameResult(
+      date: '10 Abr',
+      timeSpent: '01:20',
+      isVictory: true,
+      difficulty: 'Fácil',
+    );
+
     return Scaffold(
       appBar: AppBar(title: const Text('Historial de Partidas')),
-      body: const Center(
-        child: Text('Aquí se mostrarán las partidas anteriores.'),
+      body: Center(
+        child: GameResultUI(game: game),
       ),
     );
   }
