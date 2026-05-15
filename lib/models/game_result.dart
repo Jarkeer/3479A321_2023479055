02576be2bd2884970388bd52fa8039047669b4c9
Jarkeer <83051761+Jarkeer@users.noreply.github.com>
2026-05-15 12:03:37 -0400

    1er commit:
    Se instalo shared_preferences
    - Se agrego la una nueva prioridad llamada adjacentMines

--- lib/models/game_result.dart
+++ lib/models/game_result.dart
@@ -3,11 +3,13 @@ class GameResult {
   final String timeSpent;   
   final bool isVictory;     
   final String difficulty;  
+  final int adjacentMines;
 
   GameResult({
     required this.date,
     required this.timeSpent,
     required this.isVictory,
     required this.difficulty,
+    required this.adjacentMines,
   });
 }
